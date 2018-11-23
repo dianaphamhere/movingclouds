a67b710f74376ab3a9f2ca07d37671b908292756
Diana Pham <dianaphamis@gmail.com>
2018-11-23 13:52:20 -0800

    - some semblance of two clouds moving at different speeds. (not really optimized yet...).

--- script.js
+++ script.js
@@ -3,4 +3,37 @@ document.getElementById("demo").innerHTML = "hello world";
 var fifty = "50%";
 var eighty ="80%";
 
-document.getElementById("cloud").style.right = eighty;
+// document.getElementById("cloud").style.right = eighty;
+
+
+var small = document.getElementById("small");
+var big = document.getElementById("big");
+var w = window.innerWidth;
+var pos = 0;
+var posB = -60;
+var id = setInterval(smallFrame, 50);
+var bI = setInterval (bigInterval, 40);
+
+function smallFrame() {
+	if (pos > w) {
+		pos = -10;
+	} else {
+		pos++;
+		small.style.right = pos + "px";
+	}
+}
+
+function bigInterval() {
+	if (posB > w) {
+		posB = -10;
+	} else {
+		posB++;
+		big.style.right = posB + "px";
+	}
+}
+
+function movement(pos){
+	var random = Math.floor(Math.random() * 3);
+	pos = pos + random;
+	return pos;
+}
